bcbeb4de8870092105cc1316077a6059e5f55481
Hasan2000 <hasaniqbalnxi@gmail.com>
2024-06-13 19:46:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+let counter = 0
+
+function myfunction() {
+
+    // counter = counter + 1
+    // let shows;
+    // const input1 =  document.getElementById("inp").value ;
+
+    // if(input1 <= 18){
+    //     shows ="you r minor"
+    // }
+    // else if (input1 <= 60 && input1 >=18) {  
+    //     shows = "you r adult"
+    // }
+    // else if (input1 > 60) {
+    //     shows = "you r retired"  
+    // }
+
+
+
+
+    // document.getElementById("btn").innerHTML= shows ;
+
+    // document.getElementById("dd").value="0"
+    // 55555555
+
+    // if (document.getElementById("demo").innerHTML <= 0) {
+    //     console.log('go down')
+
+    //     if (document.getElementById("demo").innerHTML > -5) {
+    //         console.log('greater than -5')
+    //         document.getElementById("demo").innerHTML = parseInt(document.getElementById("demo").innerHTML) - 1;
+    //     } else if (document.getElementById("demo").innerHTML <= -5) {
+    //         console.log('les than -5')
+    //         document.getElementById("demo").innerHTML = parseInt(document.getElementById("demo").innerHTML) + 1;
+    //     }
+
+
+
+
+    // } else {
+    //     console.log('go up')
+    // }
+// 00000000
+
+
+
+    if (document.getElementById("demo").innerHTML <= -5) {
+         console.log(parseInt(document.getElementById("demo").innerHTML) + 1); 
+    }
+    if (document.getElementById("demo").innerHTML == -5){
+        document.getElementById("demo").innerHTML = document.getElementById("demo").innerHTML + 1
+    }
+
+
+
+    document.getElementById("demo").innerHTML = parseInt(document.getElementById("demo").innerHTML) - 1;
+
+
+}
+// function clear() {
+//     document.getElementById("btn2").innerHTML = " 0 ";
+
+
+
+// }
